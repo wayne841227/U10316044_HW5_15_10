02591f55dbdbac210d82e1cb7aa8a3670c2147d7
wayne841227 <U10316044@go.utaipei.edu.tw>
2015-04-27 20:45:37 +0800

    Create MyStack.java
    use try catch

---
+++ MyStack.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+class MyStack implements Cloneable{
+  private ArrayList<Object> list = new ArrayList<Object>();
+
+  public boolean isEmpty() {
+    return list.isEmpty();
+  }
+
+  public int getSize() {
+    return list.size();
+  }
+
+  public Object peek() {
+    return list.get(getSize() - 1);
+  }
+
+  public Object pop() {
+    Object o = list.get(getSize() - 1);
+    list.remove(getSize() - 1);
+    return o;
+  }
+
+  public void push(Object o) {
+    list.add(o);
+  }
+
+  @Override /** Override the toString in the Object class */
+  public String toString() {
+    return "stack: " + list.toString();
+  }
+  
+  	public Object clone(){
+		try{
+			MyStack mystackclone = (MyStack)super.clone();
+			mystackclone.list = (ArrayList<Object>)list.clone();
+			return mystackclone;
+		}
+		catch(CloneNotSupportedException ex){
+			return null;
+		}
+	
+	}
+  
+  
+}
